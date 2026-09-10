4a873d69ef59f02e059ae94497dbf23f8c2cc4f3
Liam Skirow <skirrowliam@gmail.com>
2026-09-10 16:30:16 +0100

    Refactored list creation to preference np arrays

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
-from neuron import Neuron
+# from neuron import Neuron
 
 # first create a configurable NN
-NUM_NEURONS_PER_LAYER    = [784,   16,   16,   10]            # needs tweaking!
+NUM_NEURONS_PER_LAYER    = [784,   128,  64,   10]            # needs tweaking!
 #                           ^ input layer   
 #                                  ^ first hidden layer
 #                                        ^ second hidden layer
@@ -17,34 +17,51 @@ weight_matrices = []
 neuron_list = []
 bias_list   = []
 
-# every layer of neurons has its own matrix to represent the weights for that layer
-# there are NUM_NEURON_LAYERS-1 many weights matrices. So if there are four neuron layers
-# there are therefore 3 separate weights matrices...
+def init_matrices():
+    # every layer of neurons has its own matrix to represent the weights for that layer
+    # there are NUM_NEURON_LAYERS-1 many weights matrices. So if there are four neuron layers
+    # there are therefore 3 separate weights matrices...
 
-# so for n weights matrices, which corresponds to n+1 neuron layers...
-# WM0 -> NUM_NEURONS_PER_LAYER[1] x NUM_NEURONS_PER_LAYER[0]
-# WM1 -> NUM_NEURONS_PER_LAYER[2] x NUM_NEURONS_PER_LAYER[1]
-# ...
-# WMn -> NUM_NEURONS_PER_LAYER[n+1] x NUM_NEURONS_PER_LAYER[n]
+    # so for n weights matrices, which corresponds to n+1 neuron layers...
+    # WM0 -> NUM_NEURONS_PER_LAYER[1] x NUM_NEURONS_PER_LAYER[0]
+    # WM1 -> NUM_NEURONS_PER_LAYER[2] x NUM_NEURONS_PER_LAYER[1]
+    # ...
+    # WMn -> NUM_NEURONS_PER_LAYER[n+1] x NUM_NEURONS_PER_LAYER[n]
 
-for i in range(0, NUM_WEIGHTS_MATRICES):
-    weight_matrices.append(np.zeros((NUM_NEURONS_PER_LAYER[i+1], NUM_NEURONS_PER_LAYER[i])))
+    for i in range(0, NUM_WEIGHTS_MATRICES):
+        # random initialisation around 0
+        weight_matrices.append(np.random.randn(NUM_NEURONS_PER_LAYER[i+1], NUM_NEURONS_PER_LAYER[i]) * 0.01)
 
-# sanity check size of weights matrices
-print('showing dimensions of weights matrices...')
-for i in range(0, NUM_WEIGHTS_MATRICES):
-    print(np.shape((weight_matrices[i])))
+def init_neuron_list():
+    # now populate the neuron list of lists
+    for i in range(0, NUM_NEURON_LAYERS):
+        neuron_layer = np.zeros(NUM_NEURONS_PER_LAYER[i])
+        neuron_list.append(neuron_layer)
 
-# now populate the neuron list of lists
-for i in range(0, NUM_NEURON_LAYERS):
-    neuron_layer = []
-    for j in range(0, NUM_NEURONS_PER_LAYER[i]):
-        neuron_layer.append(Neuron())
-    neuron_list.append(neuron_layer)
+def init_bias_list():
+    # and lastly define the bias list of lists, one bias value for each neuron, not including the input layer
+    for i in range(1, NUM_WEIGHTS_MATRICES):
+        bias_layer = np.zeros(NUM_NEURONS_PER_LAYER[i])
+        bias_list.append(bias_layer)
 
-# and lastly define the bias list of lists, one bias value for each neuron, not including the input layer
-for i in range(1, NUM_WEIGHTS_MATRICES):
-    bias_layer = []
-    for j in range(0, NUM_NEURONS_PER_LAYER[i]):
-        bias_layer.append(Neuron())
-    bias_list.append(bias_layer)
+def feedforward(input_sample):
+    # assign input_sample to input layer neurons
+    neuron_list[0] = input_sample
+
+    # apply the matrix * vector + bias operation, across all neuron layers
+    for i in range(0, NUM_WEIGHTS_MATRICES-1):
+        neuron_list[i+1] = weight_matrices[i] @ neuron_list[i] + bias_list[i]
+
+if __name__ == '__main__':
+    # initialise (TODO: with random values) the NN structural components
+    init_matrices()
+    init_neuron_list()
+    init_bias_list()
+
+    # sanity check size of weights matrices
+    print('showing dimensions of weights matrices...')
+    for i in range(0, NUM_WEIGHTS_MATRICES):
+        print(np.shape((weight_matrices[i])))
+
+    # now take a sample and feed it forward through the NN, input_sample must be np.array()
+    feedforward(np.zeros(784))
