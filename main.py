1ddf942547dc4007db8e741f93500adcf560959c
Liam Skirow <skirrowliam@gmail.com>
2026-09-10 12:23:51 +0100

    Refactor matrix sizing calculation to be more elegant and simple

--- main.py
+++ main.py
@@ -2,39 +2,40 @@ import numpy as np
 from neuron import Neuron
 
 # first create a configurable NN
-NUM_INPUT_SAMPLES  = 784
-NUM_HIDDEN_LAYERS  = 2 # probably shouldn't ever be < 1
-NUM_OUTPUT_SAMPLES = 10
-HIDDEN_LAYER_NUM_NEURONS = [10, 8] # needs thinking
-weight_matrix_hidden_layers = []
+NUM_NEURONS_PER_LAYER    = [784,   10,   8,   10]            # needs tweaking!
+#                           ^ input layer   
+#                                  ^ first hidden layer
+#                                        ^ second hidden layer
+#                                             ^ output layer
+
+NUM_NEURON_LAYERS    = len(NUM_NEURONS_PER_LAYER)    # probably shouldn't ever be < 1
+NUM_WEIGHTS_MATRICES = NUM_NEURON_LAYERS-1
+OUTPUT_LAYER_IDX     = len(NUM_NEURONS_PER_LAYER)-1
+
+# create empty structures
+weight_matrices = []
+neuron_list = []
 
 # every layer of neurons has its own matrix to represent the weights for that layer
+# there are NUM_NEURON_LAYERS-1 many weights matrices. So if there are four neuron layers
+# there are therefore 3 separate weights matrices...
 
-# the input layer weights matrix has num_cols equal to NUM_INPUT_SAMPLES
-# the input layer weights matrix has num_rows equal to HIDDEN_LAYER_NUM_NEURONS[0]
-# -> HIDDEN_LAYER_NUM_NEURONS[0] x NUM_INPUT_SAMPLES
-weight_matrix_input_layer = np.zeros((HIDDEN_LAYER_NUM_NEURONS[0], NUM_INPUT_SAMPLES))
+# so for n weights matrices, which corresponds to n+1 neuron layers...
+# WM0 -> NUM_NEURONS_PER_LAYER[1] x NUM_NEURONS_PER_LAYER[0]
+# WM1 -> NUM_NEURONS_PER_LAYER[2] x NUM_NEURONS_PER_LAYER[1]
+# ...
+# WMn -> NUM_NEURONS_PER_LAYER[n+1] x NUM_NEURONS_PER_LAYER[n]
 
-# the first hidden layer weights matrix has num_cols equal to HIDDEN_LAYER_NUM_NEURONS[0]
-# the first hidden layer weights matrix has num_rows equal to HIDDEN_LAYER_NUM_NEURONS[1]
-# -> HIDDEN_LAYER_NUM_NEURONS[1] x HIDDEN_LAYER_NUM_NEURONS[0]
-for i in range(0, NUM_HIDDEN_LAYERS-1):
-    weight_matrix_hidden_layers.append(np.zeros((HIDDEN_LAYER_NUM_NEURONS[i+1], HIDDEN_LAYER_NUM_NEURONS[i])))
+for i in range(0, NUM_WEIGHTS_MATRICES):
+    weight_matrices.append(np.zeros((NUM_NEURONS_PER_LAYER[i+1], NUM_NEURONS_PER_LAYER[i])))
 
-# the output layer weights matrix has num_cols equal to HIDDEN_LAYER_NUM_NEURONS[1]
-# the output layer weights matrix has num_rows equal to NUM_OUTPUT_SAMPLES
-weight_matrix_output_layer = np.zeros((NUM_OUTPUT_SAMPLES, HIDDEN_LAYER_NUM_NEURONS[-1]))
+# sanity check size of weights matrices
+print('showing dimensions of weights matrices...')
+for i in range(0, NUM_WEIGHTS_MATRICES):
+    print(np.shape((weight_matrices[i])))
 
-# check size of matrices
-print('input layer matrix shape')
-print(np.shape((weight_matrix_input_layer)))
-print('')
-
-print('hidden layer matrix shapes')
-for i in range(0, NUM_HIDDEN_LAYERS-1):
-    print(np.shape((weight_matrix_hidden_layers[i])))
-    print('')
-
-print('output layer matrix shape')
-print(np.shape((weight_matrix_output_layer)))
-print('')
+# now populate the neuron list of lists, NUM_HIDDEN_LAYERS+1 to account for output layer
+# for i in range(0, NUM_HIDDEN_LAYERS+1):
+#     for j in range(0, ):
+    
+#     neuron_list.append()
